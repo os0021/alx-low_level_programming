e6f8b028af809a6a05fc5e1404578b265fc67a0b
os0021 <o2142016r@icloud.com>
2022-10-14 15:22:57 -0700

    task0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,14 +7,12 @@
 
 
 /**
+ * main - prints the last digit of the random
  *
- *  * main - prints the last digit of the random
+ * number stored in the variable n
  *
- *   * number stored in the variable n
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 

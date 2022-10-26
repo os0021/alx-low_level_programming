51b35acd841b25f5a3d5b0d60a6a0df7e3fb46b4
os0021 <o2142016r@icloud.com>
2022-10-26 10:18:14 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
+ * leet - encodes a string into 1337
  *
- *  * leet - encodes a string into 1337
+ * @s: input string.
  *
- *   * @s: input string.
- *
- *    * Return: the pointer to dest.
- *
- *     */
+ * Return: the pointer to dest.
+ */
 
 
 char *leet(char *s)
@@ -17,7 +15,6 @@ char *leet(char *s)
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
-
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
@@ -26,7 +23,7 @@ char *leet(char *s)
 			{
 				*(s + count) = numbers[i];							break;
 			}
-												}
+		}
 		count++;
 	}
 	return (s);

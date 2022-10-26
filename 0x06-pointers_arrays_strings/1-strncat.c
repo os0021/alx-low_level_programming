1c33f058b1f9aa66d40f3caafa738a2d79809ea6
os0021 <o2142016r@icloud.com>
2022-10-26 09:34:26 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,6 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (*(dest + count) != '\0')
 	{
 		count++;
+	
 	}
 
 	while (count2 < n)
@@ -27,7 +28,9 @@ char *_strncat(char *dest, char *src, int n)
 		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;									count++;
+		
 		count2++;
+	
 	}
 
 	return (dest);

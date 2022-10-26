3443b5fcdb41c973319879dad553404150986697
os0021 <o2142016r@icloud.com>
2022-10-26 12:36:49 -0700

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,128 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
-
-
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-
+#include <stdio.h>
 
 /**
  *
- *  * add_strings - Adds the numbers stored in two strings.
+ *  * infinite_add - adds two numbers
  *
- *   * @n1: The string containing the first number to be added.
+ *   * @n1: number one.
  *
- *    * @n2: The string containing the second number to be added.
+ *    * @n2: number two.
  *
- *     * @r: The buffer to store the result.
+ *     * @r: buffer that the function will use to store the result.
  *
- *      * @r_index: The current index of the buffer.
+ *      * @size_r: buffer size:
  *
- *       *
+ *       * Return: the pointer to dest.
  *
- *        * Return: If r can store the sum - a pointer to the result.
- *
- *         *         If r cannot store the sum - 0.
- *
- *          */
-
-
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-
-{
-
-		int num, tens = 0;
-
-
-
-			for (; *n1 && *n2; n1--, n2--, r_index--)
-
-					{
-
-								num = (*n1 - '0') + (*n2 - '0');
-
-										num += tens;
-
-												*(r + r_index) = (num % 10) + '0';
-
-														tens = num / 10;
-
-															}
-
-
-
-				for (; *n1; n1--, r_index--)
-
-						{
-
-									num = (*n1 - '0') + tens;
-
-											*(r + r_index) = (num % 10) + '0';
-
-													tens = num / 10;
-
-														}
-
-
-
-					for (; *n2; n2--, r_index--)
-
-							{
-
-										num = (*n2 - '0') + tens;
-
-												*(r + r_index) = (num % 10) + '0';
-
-														tens = num / 10;
-
-															}
-
-
-
-						if (tens && r_index >= 0)
-
-								{
-
-											*(r + r_index) = (tens % 10) + '0';
-
-													return (r + r_index);
-
-														}
-
-
-
-							else if (tens && r_index < 0)
-
-										return (0);
-
-
-
-								return (r + r_index + 1);
-
-}
-
-/**
- *
- *  * infinite_add - Adds two numbers.
- *
- *   * @n1: The first number to be added.
- *
- *    * @n2: The second number to be added.
- *
- *     * @r: The buffer to store the result.
- *
- *      * @size_r: The buffer size.
- *
- *       *
- *
- *        * Return: If r can store the sum - a pointer to the result.
- *
- *         *         If r cannot store the sum - 0.
- *
- *          */
+ *        */
 
 
 
@@ -130,36 +24,84 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 
-		int index, n1_len = 0, n2_len = 0;
+		int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
 
 
-			for (index = 0; *(n1 + index); index++)
+			while (*(n1 + c1) != '\0')
 
-						n1_len++;
+						c1++;
 
+				while (*(n2 + c2) != '\0')
 
+							c2++;
 
-				for (index = 0; *(n2 + index); index++)
+					if (c1 >= c2)
 
-							n2_len++;
+								bg = c1;
 
+						else
 
+									bg = c2;
 
-					if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+							if (size_r <= bg + 1)
 
-								return (0);
+										return (0);
 
+								r[bg + 1] = '\0';
 
+									c1--, c2--, size_r--;
 
-						n1 += n1_len - 1;
+										dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 
-							n2 += n2_len - 1;
+											while (bg >= 0)
 
-								*(r + size_r) = '\0';
+													{
 
+																op = dr1 + dr2 + add;
 
+																		if (op >= 10)
 
-									return (add_strings(n1, n2, r, --size_r));
+																						add = op / 10;
+
+																				else
+
+																								add = 0;
+
+																						if (op > 0)
+
+																									*(r + bg) = (op % 10) + 48;
+
+																								else
+
+																												*(r + bg) = '0';
+
+																										if (c1 > 0)
+
+																														c1--, dr1 = *(n1 + c1) - 48;
+
+																												else
+
+																																dr1 = 0;
+
+																														if (c2 > 0)
+
+																																		c2--, dr2 = *(n2 + c2) - 48;
+
+																																else
+
+																																				dr2 = 0;
+
+																																		bg--, size_r--;
+
+																																			}
+
+												if (*(r) == '0')
+
+															return (r + 1);
+
+													else
+
+																return (r);
 
 }

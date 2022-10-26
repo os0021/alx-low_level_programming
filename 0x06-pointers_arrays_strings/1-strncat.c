30a67b6d7239be7f8d555825ee619e051954fbd8
os0021 <o2142016r@icloud.com>
2022-10-26 09:36:04 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,27 +11,39 @@
  *
  * Return: the pointer to dest.
  */
-
 char *_strncat(char *dest, char *src, int n)
 
 {
+
 	int count = 0, count2 = 0;
 
+
 	while (*(dest + count) != '\0')
-	{
-		count++;
-	
-	}
 
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;									count++;
-		
-		count2++;
-	
-	}
+					{
 
-	return (dest);
+								count++;
+
+									}
+
+
+
+				while (count2 < n)
+
+						{
+
+									*(dest + count) = *(src + count2);
+
+											if (*(src + count2) == '\0')
+
+															break;
+
+													count++;
+
+															count2++;
+
+																}
+
+					return (dest);
+
 }

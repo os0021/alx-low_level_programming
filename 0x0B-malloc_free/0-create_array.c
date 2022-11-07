4a790a8dcda4087d7093998086d5cfb2f3cf648a
os0021 <o2142016r@icloud.com>
2022-11-07 10:20:47 -0800

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,6 @@
  *
  * Return: pointer of an array of chars
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *cr;
@@ -19,8 +18,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	cr = malloc(sizeof(c) * size);
+	
 	if (cr == NULL)
 		return (NULL);
+	
 	for (i = 0; i < size; i++)
 		cr[i] = c;
 	return (cr);

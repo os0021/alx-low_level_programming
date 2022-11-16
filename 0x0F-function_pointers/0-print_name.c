cbd5c63170200bf6487129379b474777de09004a
os0021 <o2142016r@icloud.com>
2022-11-16 09:04:57 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,16 +3,25 @@
 
 
 /**
- * print_name - prints a name.
  *
- * @name: input name.
+ *  * print_name - prints a name.
  *
- * @f: function pointer.
+ *   * @name: input name.
  *
- * Return: no return.
- */
+ *    * @f: function pointer.
+ *
+ *     *
+ *
+ *      * Return: no return.
+ *
+ *       */
+
 void print_name(char *name, void (*f)(char *))
+
 {
-	if (name && f)
-		f(name);
+
+		if (name && f)
+
+					f(name);
+
 }
